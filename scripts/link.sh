c7f3ae427d8b1465a35e898d414d36163f588090
Pasha Kuznetsov <pasha@tydbits.com>
2026-05-14 00:00:18 -0700

    refactor: docs submodule

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,6 +2,14 @@
 set -Eeo pipefail
 cd "$(dirname "$0")/.."
 
+pushd docs > /dev/null
+ln -sf ../vars.json ./
+popd > /dev/null
+
+pushd docs/assets > /dev/null
+ln -sf ../../assets/*.png ./
+popd > /dev/null
+
 pushd server > /dev/null
 ln -sf ../wrangler.jsonc ./
 ln -sf ../worker-configuration.d.ts ./
