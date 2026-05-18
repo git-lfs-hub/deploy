4c13069e2ea171798ac30375e828759538b1fea0
Pasha Kuznetsov <pasha@tydbits.com>
2026-05-17 23:56:30 -0700

    fix(ci): copy in CI - still symlink in local dev

--- scripts/sync-docs.sh
+++ scripts/sync-docs.sh
@@ -5,7 +5,13 @@ cd "$(dirname "$0")/.."
 # Update turbo.json whenever changing theese
 
 pushd docs > /dev/null
-ln -sf ../vars.resolved.json vars.json
+if [ -z "$CI" ]; then
+    # Symlink locally for live updates
+    ln -sf ../vars.resolved.json vars.json
+else
+    # copy in CI because turbo doesn't restore symlinks from cache
+    rsync -avh ../vars.resolved.json vars.json
+fi
 popd > /dev/null
 
 rsync -avh assets/ docs/assets
