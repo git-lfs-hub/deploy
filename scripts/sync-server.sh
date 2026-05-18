50cfc46a144ce1428efe5912b61d110480fc15df
Pasha Kuznetsov <pasha@tydbits.com>
2026-05-18 00:11:14 -0700

    fix(ci): copy in CI (#2)
    * fix(ci): copy in CI
    - still symlink in local dev
    - fix vars#init paths (relative to vars/)

--- scripts/sync-server.sh
+++ scripts/sync-server.sh
@@ -5,8 +5,17 @@ cd "$(dirname "$0")/.."
 # Update turbo.json whenever changing theese
 
 pushd server > /dev/null
-ln -sf ../wrangler.jsonc ./
-ln -sf ../worker-configuration.d.ts ./
-ln -sf ../vars.resolved.json vars.json
-ln -sfn ../docs/site public
+if [ -z "$CI" ]; then
+    # Symlink locally for live updates
+    ln -sf ../wrangler.jsonc ./
+    ln -sf ../worker-configuration.d.ts ./
+    ln -sf ../vars.resolved.json vars.json
+    ln -sfn ../docs/site public
+else
+    # copy in CI because turbo doesn't restore symlinks from cache
+    rsync -avh ../wrangler.jsonc ./
+    # rsync -avh ../worker-configuration.d.ts ./
+    rsync -avh ../vars.resolved.json vars.json
+    rsync -avh ../docs/site/ public
+fi
 popd > /dev/null
